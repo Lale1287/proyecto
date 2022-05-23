a6af82bc48ba9fbdc9ac4d3ab259554fe6622339
Lale1287 <alejandracolombini1287@gmail.com>
2022-05-22 22:16:38 -0300

    menu hamburguesa v1

---
+++ js/menuhamb.js
@@ -0,0 +1,13 @@
+document.querySelector('#button-menu').addEventListener('click', animateBars);
+
+var linea1_barra = document.querySelector('.linea1');
+var linea2_barra = document.querySelector('.linea2');
+var linea3_barra = document.querySelector('.linea3');
+const container_menu = document.querySelector('.navMenu');
+
+function animateBars(){
+    linea1_barra.classList.toggle('active-linea1');
+    linea2_barra.classList.toggle('active-linea2');
+    linea3_barra.classList.toggle('active-linea3');
+    container_menu.classList.toggle('menu-active');
+}
