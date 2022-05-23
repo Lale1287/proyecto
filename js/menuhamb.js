049216b094e3117e42923721b34c1a427ec3690e
Lale1287 <alejandracolombini1287@gmail.com>
2022-05-23 00:42:36 -0300

    altura y formatos alineacion general

--- js/menuhamb.js
+++ js/menuhamb.js
@@ -3,7 +3,7 @@ document.querySelector('#button-menu').addEventListener('click', animateBars);
 var linea1_barra = document.querySelector('.linea1');
 var linea2_barra = document.querySelector('.linea2');
 var linea3_barra = document.querySelector('.linea3');
-const container_menu = document.querySelector('.navMenu');
+const container_menu = document.querySelector('.nav-links');
 
 function animateBars(){
     linea1_barra.classList.toggle('active-linea1');
